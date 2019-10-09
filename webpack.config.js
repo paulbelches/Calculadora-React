7203548e212d51808f84c39b88dbd1f5269d1e77
paulbelches <paulbelches@gmail.com>
2019-10-09 14:45:19 -0600

    Boiler plate

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+module.exports = {
+    module: {
+        rules: [
+                {
+                  test: /\.(js|jsx)$/,
+                  exclude: /node_modules/,
+                  use: [ "babel-loader", "eslint-loader"]
+                  
+                },
+            {
+                test: /\.html$/,
+                use : [
+                    {
+                        loader: "html-loader",
+                        options: {minisize: true}
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+}
